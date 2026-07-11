cdae5f5227ba454ca711650d97eebf8c87b1ed24
LGRSV <joaoantonio.negocios@gmail.com>
2026-07-11 18:09:27 -0300

    chore(pwa): distribuir filtros financeiros

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'agenda-lagares-v127-split-equipment';
+const CACHE = 'agenda-lagares-v128-finance-filters';
 const EDIT_SCRIPT = '<script src="./edit-enhancement.js?v=3"><\/script>';
 const TREINO_SCRIPT = '<script src="./treino.js?v=8"><\/script>';
 const PAINEL_SCRIPT = '<script src="./painel.js?v=2"><\/script>';
@@ -86,4 +86,4 @@ self.addEventListener('notificationclick', event => {
   })());
 });
 
-// redeploy trigger v127-split-equipment
+// redeploy trigger v128-finance-filters
