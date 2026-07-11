dc19aae5d82f9340e4a7e326a96a17f8ad5ac6e4
LGRSV <joaoantonio.negocios@gmail.com>
2026-07-11 18:12:48 -0300

    chore(pwa): distribuir cores financeiras

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'agenda-lagares-v128-finance-filters';
+const CACHE = 'agenda-lagares-v129-finance-colors';
 const EDIT_SCRIPT = '<script src="./edit-enhancement.js?v=3"><\/script>';
 const TREINO_SCRIPT = '<script src="./treino.js?v=8"><\/script>';
 const PAINEL_SCRIPT = '<script src="./painel.js?v=2"><\/script>';
@@ -86,4 +86,4 @@ self.addEventListener('notificationclick', event => {
   })());
 });
 
-// redeploy trigger v128-finance-filters
+// redeploy trigger v129-finance-colors
