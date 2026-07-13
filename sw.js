6d0a55eb37b293e1abfd9a4a0b4b4b0a6f39347a
LGRSV <joaoantonio.negocios@gmail.com>
2026-07-13 07:16:44 -0300

    feat: lista de pendentes e progresso diário

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'agenda-lagares-v176-lista-data-selecionada';
+const CACHE = 'agenda-lagares-v177-lista-pendentes-progresso-diario';
 const EDIT_SCRIPT = '<script src="./edit-enhancement.js?v=4"><\/script>';
 const TREINO_SCRIPT = '<script src="./treino.js?v=10"><\/script>';
 const PAINEL_SCRIPT = '<script src="./painel.js?v=2"><\/script>';
