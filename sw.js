c055f30a03b4fc62aa456baf58ade4d926db0c98
LGRSV <joaoantonio.negocios@gmail.com>
2026-07-13 05:53:45 -0300

    feat: color full-screen calendar tasks by category

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'agenda-lagares-v171-stacked-agenda-actions';
+const CACHE = 'agenda-lagares-v172-full-calendar-categories';
 const EDIT_SCRIPT = '<script src="./edit-enhancement.js?v=4"><\/script>';
 const TREINO_SCRIPT = '<script src="./treino.js?v=9"><\/script>';
 const PAINEL_SCRIPT = '<script src="./painel.js?v=2"><\/script>';
